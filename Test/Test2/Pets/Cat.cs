8aa4efddf85a9e5685aef9dcd9125b04a191926b
Tristan Harms <33315890+tristanharms-cb@users.noreply.github.com>
2018-11-05 11:10:55 +0100

    Revert "Third PR - Add dogs to pets"

--- Test/Test2/Pets/Cat.cs
+++ Test/Test2/Pets/Cat.cs
@@ -2,18 +2,28 @@
 
 namespace Test2.Pets
 {
-    public class Cat : IPet
-    {
-        private PetColor Color { get; }
+    public interface IPet
+    {        
+        void MakeSound();
+    }
 
+    public abstract class PetBase
+    {
+        public virtual PetColor Color => PetColor.White;
+    }
+
+    public class Cat : PetBase, IPet
+    {
         public Cat(PetColor color)
         {
             Color = color;
         }
 
+        public override PetColor Color { get; }
+
         public void MakeSound()
         {
-            Console.WriteLine($"{Color} cat says meow!");
+            Console.WriteLine("Meow!");
         }
     }
 }
