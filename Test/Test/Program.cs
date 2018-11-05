5e8cf0ec7b10fdf68363fc0f29b426a08aed8338
Tristan Harms <tristan.harms@coolblue.nl>
2018-11-05 11:03:21 +0100

    Let's re-introduce cats

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Test
+﻿using Test2.Pets;
+
+namespace Test
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            IPet blackTabby = new Cat(PetColor.Black);
+            blackTabby.MakeSound();
         }
     }
 }
